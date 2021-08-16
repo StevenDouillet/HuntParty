dae8f563190bd37bcd6255d0e22db887d529e230
DOUILLET <Steven.douillet@gmail.com>
2021-08-16 16:08:01 +0200

    init project with acf

---
+++ src/main/java/fr/edencraft/huntparty/HuntParty.java
@@ -0,0 +1,14 @@
+package fr.edencraft.huntparty;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HuntParty extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
